1d2217ba69e9dfd458485f0bbbc9980df75391cc
Aleks Rūtiņš <aleks@rutins.com>
2022-04-13 07:45:13 -0400

    Initial Commit

---
+++ RailwayDesktop/RailwayDesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  RailwayDesktopApp.swift
+//  RailwayDesktop
+//
+//  Created by Student on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct RailwayDesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
